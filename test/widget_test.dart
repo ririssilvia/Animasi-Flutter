7a6fdb5857e2f5e8ca8e93f698b4cf851a65825e
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-09-20 10:37:25 +0700

    memchkan beberapa clas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:annimation_flutter/main.dart';
+import 'package:annimation_flutter/main1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
